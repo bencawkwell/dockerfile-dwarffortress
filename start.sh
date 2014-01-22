1dc011cd3d47e56616af69789e6062ccef0653e3
Ben Cawkwell <bencawkwell@gmail.com>
2014-01-22 18:15:11 +0100

    Fix whitespace and remove useless code

--- start.sh
+++ start.sh
@@ -22,33 +22,28 @@ INTRO="YES"
 PRINTMODE="2D"
 while getopts “:hp:sx:” OPTION
 do
-     case $OPTION in
-         h)
-             usage
-             exit 1
-             ;;
-         p)
-             PRINTMODE=$OPTARG
-             ;;
-         s)
-             INTRO="NO"
-             ;;
-         x)
-             $OPTARG
-             exit
-             ;;
-         ?)
-             usage
-             exit
-             ;;
-     esac
+    case $OPTION in
+        h)
+            usage
+            exit 1
+            ;;
+        p)
+            PRINTMODE=$OPTARG
+            ;;
+        s)
+            INTRO="NO"
+            ;;
+        x)
+            $OPTARG
+            exit
+            ;;
+        ?)
+            usage
+            exit
+            ;;
+    esac
 done
 
-if [[ -z $PRINTMODE ]]
-then
-     usage
-     exit 1
-fi
 sed -i -r "s/^\[INTRO:.+\]/[INTRO:$INTRO]/" df_linux/data/init/init.txt
 sed -i -r "s/^\[PRINT_MODE:.+\]/[PRINT_MODE:$PRINTMODE]/" df_linux/data/init/init.txt
 df_linux/dfhack
