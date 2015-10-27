179fac9f2730cad64ee29b5c9a79582291c9a8f0
Ben Cawkwell <bencawkwell@gmail.com>
2015-10-27 23:39:03 +0100

    Now dwarffortress has been added to the PATH so no need to check for dfhack in start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,4 @@ if [ -e "/usr/bin/supervisord" ] ; then
     /wait-for-daemons.sh -a
 fi
 
-if [ -e "/df_linux/dfhack" ] ; then
-    DF_PATH=/df_linux/dfhack
-else
-    DF_PATH=/df_linux/df
-fi
-
-DISPLAY=:1 $DF_PATH
+DISPLAY=:1 dwarffortress
