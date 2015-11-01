3cdd7cfc2162b876dbfd7fed9a968fe65b2c6ce4
Ben Cawkwell <bencawkwell@gmail.com>
2015-11-01 23:02:13 +0100

    Since the save directory is a mountable volume, it looses its ownership when the container is started, and need to be set to the df user again on startup

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ if [ -e "/usr/bin/supervisord" ] ; then
     service supervisor start
     /wait-for-daemons.sh -a
 fi
+chown -R df:df /df_linux/data/save
 sudo -u df -H DISPLAY=:1 dwarffortress "$@"
